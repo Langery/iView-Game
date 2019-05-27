b727ee56d3b7f9b858aba25e94b0bda271ca1471
Langery <1245066197@qq.com>
2019-05-27 19:15:43 +0800

    rewrite eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,35 @@
+// 配置eslint
 module.exports = {
   root: true,
 
   env: {
-    node: true,
+    // node: true,
+    browser: true,
   },
-
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
+    'standard'
   ],
 
   rules: {
-    'no-console': 'off',
-    'no-debugger': 'off',
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 禁用不必要的转义字符
+    "no-useless-escape": 0
   },
 
   parserOptions: {
     parser: 'babel-eslint',
+    //设置"script"（默认）或"module"如果你的代码是在ECMAScript中的模块。
+    sourceType: 'module'
   },
 
-  'extends': [
-    'plugin:vue/recommended',
-    '@vue/airbnb'
+  // required to lint *.vue files
+  plugins: [
+    'vue'
   ]
-};
+
+}
